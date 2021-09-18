d6c1646631d5760d66e370ff5106b9b1420c3f37
Julia Gafina <y28yulia@gmail.com>
2021-09-19 00:25:09 +0500

    add frontend

--- ClientApp/src/App.vue
+++ ClientApp/src/App.vue
@@ -1,32 +1,111 @@
 <template>
   <div id="app">
-    {{ weather }}
+    <camera-angle />
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+// import axios from 'axios'
+import CameraAngle from './components/CameraAngle.vue'
 export default {
   name: 'App',
+  components: {
+    CameraAngle
+  },
   data() {
     return {
       weather: {}
     }
   },
   created() {
-    axios.get('/api/weatherforecast')
-      .then(res => this.weather = res.data)
+    // axios.get('/api/weatherforecast')
+    //   .then(res => this.weather = res.data)
   },
 }
 </script>
 
-<style>
+<style lang="scss">
+body {
+  margin: 0;
+  background: #f8f8f8;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 30px auto;
+  max-width: 630px;
+}
+input {
+  background: #FFFFFF;
+  border: 1px solid #d8d8d8;
+  height: 40px;
+  border-radius: 8px;
+  padding: 0 16px;
+  outline: none;
+  font-size: 16px;
+
+  &::-webkit-outer-spin-button,
+  &::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+  &[type=number] {
+    -moz-appearance: textfield;
+  }
+
+  &::placeholder {
+    color: #79818c;
+  }
+  &:hover {
+    border-color: #79818c;
+  }
+  &:not(:placeholder-shown) {
+    border-color: #818181;
+  }
+  &:focus, &:active {
+    border-color: #367ee7;
+    &::placeholder {
+      color: #d1d1d1;
+    }
+  }
+}
+
+label {
+  color: #79818c;
+  font-size: 16px;
+  margin-bottom: 8px;
+}
+
+button {
+  color: #1f1f25;
+  font-size: 16px;
+  font-weight: 700;
+  padding: 11px 60px;
+  height: 40px;
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+
+  &.primary {
+    background-color: #ffd400;
+    &:hover {
+      background-color: #fdca21;
+    }
+    &:active {
+      background-color: #ffd138;
+    }
+  }
+  &.secondary {
+    background-color: #f1f5f9;
+    &:hover {
+      background-color: #e5eff9;
+    }
+    &:active {
+      background-color: #eaf1f9;
+    }
+  }
 }
 </style>
---
+++ ClientApp/src/components/CameraAngle.vue
@@ -0,0 +1,182 @@
+<template>
+  <div class="calculator">
+    <div class="camera-params">
+      <h1>Рассчет вертикального угла наклона камеры</h1>
+      <img class="info-img" src="@/assets/image.png">
+      <div class="params">
+        <div class="param">
+          <label for="object">Расстояние до объекта (A, см)</label>
+          <input
+            v-model="distToObject"
+            name="object"
+            type="number"
+            @change="clearAnswer"
+          >
+        </div>
+        <div class="param">
+          <label for="camera">Высота установки камеры (D, см)</label>
+          <input
+            v-model="cameraHeight"
+            name="camera"
+            type="number"
+            @change="clearAnswer"
+          >
+        </div>
+      </div>
+      <div class="buttons">
+        <button
+          class="btn secondary"
+          @click="clearData(); clearAnswer()"
+        >Сбросить</button>
+        <button
+          class="btn primary"
+          @click="calculateAngle"
+        >Рассчет</button>
+      </div>
+    </div>
+    <transition
+      name="showanswer"
+      v-on:enter="scrollToAnswer"
+    >
+      <div v-if="answer" class="answer">
+        <div><b>Высота над человеком (B):</b> {{ heightAbovePerson }}см</div>
+        <div><b>Угол наклона:</b> {{ answer }}°</div>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CameraAngle',
+  data() {
+    return {
+      distToObject: 0,
+      cameraHeight: 0,
+      answer: null,
+      heightAbovePerson: null
+    }
+  },
+  methods: {
+    clearData() {
+      this.distToObject = 0
+      this.cameraHeight = 0
+    },
+    clearAnswer() {
+      this.answer = null
+      this.heightAbovePerson = null
+    },
+    calculateAngle() {
+      this.answer = this.distToObject * this.cameraHeight
+      this.heightAbovePerson = parseInt(this.distToObject) + parseInt(this.cameraHeight)
+    },
+    scrollToAnswer() {
+      window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: "smooth"
+      })
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.calculator {
+  margin: 0 30px;
+}
+.camera-params {
+  margin: 0 auto;
+  border: 1px solid #d8d8d8;
+  border-radius: 10px;
+  background: #fff;
+  padding: 30px;
+  z-index: 1;
+
+  h1 {
+    margin: 0 0 25px;
+  }
+
+  .info-img {
+    width: 70%;
+  }
+}
+
+.params {
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  margin: 30px 0;
+  
+  .param {
+    display: flex;
+    flex-direction: column;
+    max-width: 300px;
+    text-align: left;
+    &:last-child {
+      margin-left: auto;
+    }
+  }
+}
+
+.buttons {
+  display: flex;
+  justify-content: center;
+  .btn:last-child {
+    margin-left: auto;
+  }
+}
+
+.answer {
+  margin: 0 auto;
+  border: 1px solid #d8d8d8;
+  border-top: none;
+  border-radius: 0 0 10px 10px;
+  background: #fff;
+  width: 50%;
+  padding: 20px 10px 10px;
+  background: #dfffbf;
+
+  div {
+    margin: 0 0 10px;
+  }
+}
+
+.showanswer-enter-active,
+.showanswer-leave-active {
+  transition: all .8s ease;
+  position: relative;
+  z-index: -1;
+}
+.showanswer-enter, .showanswer-leave-to {
+  transform: translateY(-100%);
+  transform-origin: 50% 0;
+}
+
+@media screen and (max-width: 620px) {
+  .params {
+    flex-direction: column;
+    align-items: center;
+
+    .param {
+      width: 100%;
+      
+      &:last-child {
+        margin: 15px 0 0;
+      }
+    }
+  }
+}
+
+@media screen and (max-width: 530px) {
+  .buttons {
+    flex-wrap: wrap;
+    .btn {
+      width: 70%;
+      padding: 0 5px;
+      &:last-child {
+        margin: 15px 0 0;
+      }
+    }
+  }
+}
+</style>
